bd573d4f621de1531f289628ff9015a96733f5f2
aMoorman <austin.moorman08@gmail.com>
2020-05-13 11:36:51 -0500

    fix port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.HTTP_PORT || 3005;
+const PORT = process.env.PORT || 3005;
 
 
 require('dotenv').config()
